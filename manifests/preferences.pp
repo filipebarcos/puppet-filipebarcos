64742f14e7033610931cd0016b1501ae75002ed3
Filipe Costa <filipebarcos@gmail.com>
2015-05-31 09:49:18 -0500

    Fix keyboard repeat rate + other configs

--- manifests/preferences.pp
+++ manifests/preferences.pp
@@ -2,12 +2,22 @@
 class filipebarcos::preferences {
   notice('Setting up osx preferences for filipebarcos')
   include sourcecodepro
-  include osx::dock::autohide
-  include osx::global::disable_autocorrect
-  include osx::global::expand_save_dialog
+
+  include osx::global::disable_key_press_and_hold
   include osx::global::key_repeat_rate
+  include osx::global::enable_keyboard_control_access
+  include osx::global::expand_save_dialog
+  include osx::global::expand_print_dialog
+  include osx::global::disable_autocorrect
+
+  include osx::dock::autohide
+
   include osx::finder::show_all_on_desktop
+  include osx::finder::unhide_library
   include osx::finder::enable_quicklook_text_selection
+
+  include osx::safari::enable_developer_mode
+
   include osx::disable_app_quarantine
   include osx::no_network_dsstores
   include osx::keyboard::capslock_to_control
